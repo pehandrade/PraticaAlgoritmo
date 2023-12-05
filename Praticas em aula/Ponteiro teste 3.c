e27aeaef6d25c0eca613bc6253866e69e1fa4d47
Pedro Andrade <65511680+pehandrade@users.noreply.github.com>
2023-12-05 15:05:17 -0300

    Update Ponteiro teste 3.c

--- Praticas em aula/Ponteiro teste 3.c
+++ Praticas em aula/Ponteiro teste 3.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <conio.h>
 int a,b;
-int soma(a,  b){
+int soma(int a, int b){
     return a + b;
 }
-int subtracao(a, b){
+int subtracao(int a, int b){
     return a - b;
 }
-int multiplicacao(a, b){
+int multiplicacao(int a, int b){
     return a * b;
 }
-int divicao(a, b){
+int divicao(int a, int b){
     return a / b;
 }
 
@@ -18,26 +19,45 @@ int calcula(int x, int y, int (*operacao)(int, int)){
 }
 
 int main(){
+    int c,d;
     int opcao;
     int resultado;
     printf("escolha a operacao desejada:\n 1=soma\n2=subtracao\n3=multiplicacao\n4=divicao\n");
-    scanf("%d",opcao);
+    scanf("%d",&opcao);
     switch (opcao){
     case 1:
-        resultado = calcula(5, 3, soma);
+        printf("escolha o primeiro numero:\n");
+        scanf("%d",&c);
+        printf("escolha o segundo numero:\n");
+        scanf("%d",&d);
+        resultado = calcula(c, d, soma);
         printf("resultado: %d",resultado);
         break;
     
     case 2:
-        resultado = calcula(5, 3, subtracao);
+        printf("escolha o primeiro numero:\n");
+        scanf("%d",&c);
+        printf("escolha o segundo numero:\n");
+        scanf("%d",&d);
+        resultado = calcula(c, d, subtracao);
         printf("resultado: %d",resultado);
         break;
     case 3:
-        resultado = calcula(5, 3, multiplicacao);
+        printf("escolha o primeiro numero:\n");
+        scanf("%d",&c);
+        printf("escolha o segundo numero:\n");
+        scanf("%d",&d);
+        resultado = calcula(c, d, multiplicacao);
         printf("resultado: %d",resultado);
+        break;
     case 4:
-        resultado = calcula(5, 3, divicao);
+        printf("escolha o primeiro numero:\n");
+        scanf("%d",&c);
+        printf("escolha o segundo numero:\n");
+        scanf("%d",&d);
+        resultado = calcula(c, d, divicao);
         printf("resultado: %d",resultado);
+        break;
     }
     return 0;
 }
